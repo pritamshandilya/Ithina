b932966989c438e1125cb81b9d1a06dbf4a471b1
Melvin Otieno <o.melvinotieno@gmail.com>
2025-10-14 12:12:19 +0300

    feat: refactor App component to integrate React Query and TanStack Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { routeTree } from "./routeTree.gen";
 
+const queryClient = new QueryClient();
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+  scrollRestoration: true,
+  defaultPreloadStaleTime: 0,
+  context: {
+    queryClient,
+    auth: undefined!,
+  },
+});
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
 
-export default App
+      {Boolean(import.meta.env.VITE_DEBUG) && (
+        <>
+          <ReactQueryDevtools />
+
+          <TanStackRouterDevtools router={router} />
+        </>
+      )}
+    </QueryClientProvider>
+  );
+}

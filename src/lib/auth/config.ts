35a9b723ab8431f80c61742b6bd2ee3d450b4387
Melvin Otieno <o.melvinotieno@gmail.com>
2025-10-14 14:24:14 +0300

    feat: add AuthConfig interface for authentication configuration

---
+++ src/lib/auth/config.ts
@@ -0,0 +1,15 @@
+export interface AuthConfig {
+  serverUrl: string;
+  loginPath?: string;
+  registerPath?: string;
+  logoutPath?: string;
+  tokenRefreshPath?: string;
+  userInfoPath?: string;
+  accountManagementPath?: string;
+  accessTokenExpiryCookieName?: string;
+  shouldAutoRefresh?: boolean;
+  shouldAutoFetchUserInfo?: boolean;
+  shouldTimeoutSession?: boolean;
+  shouldInvalidateSession?: boolean;
+  onTokenExpiration: () => void;
+}

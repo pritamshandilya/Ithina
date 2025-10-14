0292af9634d9234f442f4a9e058951bbcff63b38
Melvin Otieno <o.melvinotieno@gmail.com>
2025-10-14 14:18:08 +0300

    feat: implement AuthProvider component for authentication context management

---
+++ src/providers/auth/provider.tsx
@@ -0,0 +1,101 @@
+import type { PropsWithChildren } from "react";
+import { useMemo, useRef, useState } from "react";
+
+import type { AuthProviderContext, UserInfo } from "./context";
+import { AuthContext } from "./context";
+import { useRedirecting } from "./hooks/useRedirecting";
+import { useTokenRefresh } from "./hooks/useTokenRefresh";
+import { useUserInfo } from "./hooks/useUserInfo";
+import { Auth, type AuthConfig } from "@/lib/auth";
+
+export function AuthProvider(props: PropsWithChildren<AuthConfig>) {
+  const config: Omit<AuthConfig, "onTokenExpiration"> = useMemo(
+    () => ({
+      serverUrl: props.serverUrl,
+      loginPath: props.loginPath,
+      registerPath: props.registerPath,
+      logoutPath: props.logoutPath,
+      tokenRefreshPath: props.tokenRefreshPath,
+      userInfoPath: props.userInfoPath,
+      accessTokenExpiryCookieName: props.accessTokenExpiryCookieName,
+      shouldAutoRefresh: props.shouldAutoRefresh,
+      shouldAutoFetchUserInfo: props.shouldAutoFetchUserInfo,
+    }),
+    [
+      props.serverUrl,
+      props.loginPath,
+      props.registerPath,
+      props.logoutPath,
+      props.tokenRefreshPath,
+      props.userInfoPath,
+      props.accessTokenExpiryCookieName,
+      props.shouldAutoRefresh,
+      props.shouldAutoFetchUserInfo,
+    ],
+  );
+
+  const authRef = useRef<Auth>(null);
+
+  const auth: Auth = useMemo(() => {
+    if (authRef.current) {
+      authRef.current.dispose();
+    }
+
+    const newAuth = new Auth({
+      ...config,
+      onTokenExpiration: () => setIsLoggedIn(false),
+    });
+
+    authRef.current = newAuth;
+
+    return newAuth;
+  }, [config]);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(auth.isLoggedIn);
+
+  const { userInfo, isFetchingUserInfo, error, fetchUserInfo } =
+    useUserInfo<UserInfo>(auth, config.shouldAutoFetchUserInfo ?? false);
+
+  const { manageAccount, startLogin, startRegister, startLogout } =
+    useRedirecting(auth);
+
+  const { refreshToken, initAutoRefresh } = useTokenRefresh(
+    auth,
+    config.shouldAutoRefresh ?? false,
+  );
+
+  const contextValue: AuthProviderContext = useMemo(
+    () => ({
+      isLoggedIn,
+      userInfo,
+      isFetchingUserInfo,
+      error,
+      fetchUserInfo,
+      startLogin,
+      startRegister,
+      startLogout,
+      manageAccount,
+      refreshToken,
+      initAutoRefresh,
+    }),
+    [
+      isLoggedIn,
+      userInfo,
+      isFetchingUserInfo,
+      error,
+      fetchUserInfo,
+      startLogin,
+      startRegister,
+      startLogout,
+      manageAccount,
+      refreshToken,
+      initAutoRefresh,
+    ],
+  );
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+}

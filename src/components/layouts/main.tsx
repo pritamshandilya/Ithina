4f01b6d96899d953a1889cd89a17f33eb37baa2d
Melvin Otieno <o.melvinotieno@gmail.com>
2025-11-28 16:16:01 +0300

    feat: replace Sidebar component with Sidenav in MainLayout

--- src/components/layouts/main.tsx
+++ src/components/layouts/main.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "@tanstack/react-router";
 
 import Header from "./main/header";
-import Sidebar from "./main/sidebar";
+import Sidenav from "./main/sidenav";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 export default function MainLayout() {
   return (
     <SidebarProvider>
-      <Sidebar />
+      <Sidenav />
 
       <SidebarInset>
         <Header />

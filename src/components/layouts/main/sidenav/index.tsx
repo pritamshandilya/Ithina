fc5c5d9976a283c71420890761ae40d16c7b7601
rohitp-cba <rohit.patel@creativebitsai.com>
2026-02-20 12:12:14 +0530

    fix: alignment and added seperator

--- src/components/layouts/main/sidenav/index.tsx
+++ src/components/layouts/main/sidenav/index.tsx
@@ -16,6 +16,7 @@ import {
 import { useEffect, useMemo, useState } from "react";
 
 import { StoreSelectorDropdown } from "@/components/checker/store-selector-dropdown";
+import { Separator } from "@/components/ui/separator";
 import { useStore } from "@/providers/store";
 import { useStores as useMakerStores } from "@/features/maker/hooks";
 import { useStores as useCheckerStores } from "@/features/checker/hooks";
@@ -134,8 +135,8 @@ export default function Sidenav() {
   return (
     <Sidebar collapsible="icon">
       <SidebarContent>
-        <SidebarGroup className="group-data-[collapsible=icon]:hidden">
-          <SidebarGroupContent >
+        <SidebarGroup className="group-data-[collapsible=icon]:hidden p-0 h-12 px-2">
+          <SidebarGroupContent className="pt-2">
             <StoreSelectorDropdown
               stores={stores ?? []}
               selectedStoreId={selectedStore?.id ?? ""}
@@ -144,6 +145,7 @@ export default function Sidenav() {
             />
           </SidebarGroupContent>
         </SidebarGroup>
+        <Separator className="group-data-[collapsible=icon]:hidden" />
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
